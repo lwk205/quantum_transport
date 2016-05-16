291437db0acaea6b2ce2cae55297c47dfffa3e23
rainson <rainson2012@qq.com>
2016-05-16 17:27:19 +0800

    map_function

--- bpfirst.py
+++ bpfirst.py
@@ -22,10 +22,10 @@ class SimplenameSpace:
         self.W = 0.0
         self.EU = 0.0
         self.ED = 0.0
-        self.vtop = 0.0
-        self.vbottom = 0.0
-        self.vi = 0.0
-        self.EF = 0.0
+        self.vtop = 0.5
+        self.vbottom = -0.5
+        self.vi = 0.01
+        self.EF = 0.25
 
 
 def attr_vers_cond(sys,pars,values,ax,attr):
@@ -33,56 +33,63 @@ def attr_vers_cond(sys,pars,values,ax,attr):
         calculate.calculate_conductance(sys, Ef=pars.EF, out_leads=1, in_leads=0, pars=pars)
         for vars(pars)[attr]  in values
         ]
-    ax = draw.simple_plot_data_2d([values, Ts], ax=ax, xlabel=attr, ylim=[-0.1, 6.1])
+    ax = draw.simple_plot_data_2d([values, Ts], ax=ax, xlabel=attr)
     return ax
 
 
 
 def main():
-    Nx = 10; Ny = 30;
+    Nx = 20; Ny = 40;
     
     sys, lead = monolayer_black_phosphorus.make_system_two_terminal(Nx, Ny)
-    pars = SimplenameSpace()
-    pars.vtop = 0.5
-    pars.vbottom = -pars.vtop
-    pars.vi = 0.01
-    pars.EF = 0.35
+
+#    pars.vtop = 0.5
+#    pars.vbottom = -pars.vtop
+#    pars.vi = 0.01
+#    pars.EF = 0.35
 
     ##plot_bands
     # bands = calculate.caculate_energy_band(lead,pars)
     # draw.simple_plot_data_2d(bands,axes[0],xlabel="k")
 
-    fig, axes = plt.subplots(1,4)
+    fig, axes = plt.subplots(1,3)
     ##磁场EF变化
-    pars.EL, pars.ER, pars.W = [0.0, 0.0, 0.0]
-    phis = np.linspace(-3, 3, 10)
-    attr_vers_cond(sys,pars,phis,axes[0],'EF')
+    pars = SimplenameSpace()
+    pars.EL, pars.ER, pars.W = [-0.3, -0.6, 0.0]
+    EFs = np.linspace(0.0, 0.6 , 100)
+    attr_vers_cond(sys,pars,EFs,axes[0],'EF')
 
-    ##磁场phi变化
-    pars.EL, pars.ER, pars.W = [-0.01, 0.0, 0.0]
-    phis = np.linspace(-3, 3, 10)
-    attr_vers_cond(sys,pars,phis,axes[1],'phi')
+#    ##磁场phi变化
+#    pars = SimplenameSpace()
+#    pars.EL, pars.ER, pars.W = [0.0, 0.0, 0.0]
+#    phis = np.linspace(-3, 3, 50)
+#    attr_vers_cond(sys,pars,phis,axes[1],'phi')
 
-    ##右边势能变化
-    pars.EL, pars.W ,pars.phi = [0,0,0.02]
-    ERs = np.linspace(-2.1, 2.1, 50)
-    attr_vers_cond(sys, pars, ERs, axes[2], 'ER')
+    ##右边势能ER变化    
+    pars = SimplenameSpace()
+    pars.EF = 0.0
+    pars.EL, pars.W ,pars.phi = [-0.3, 0.0, 0.00]
+    ERs = np.linspace(-3.0, 0.3, 100)
+    attr_vers_cond(sys, pars, ERs, axes[1], 'ER')
 
-    ##无序强度变化
-    pars.EL,pars.ER,pars.phi=[0,0,0]
-    Ws = np.linspace(0, 1.0, 10)
-    attr_vers_cond(sys, pars, Ws, axes[3], 'W')
+    ##无序强度W变化
+    pars = SimplenameSpace()
+    pars.EF = 0.0
+    pars.EL,pars.ER,pars.phi=[-0.3, -0.6, 0.00]
+    Ws = np.linspace(0, 5.0, 100)
+    attr_vers_cond(sys, pars, Ws, axes[2], 'W')
     
     fig.set_figwidth(12)
     fig.set_figheight(3)
-
-    wave_density = calculate.wave_density(sys,0.4,pars,lead_nr=0)
-    kwant.plotter.map(sys,wave_density)
-    #plt.show()
+#
+#    pars = SimplenameSpace()
+#    wave_density = calculate.wave_density(sys,pars.EF,pars,lead_nr=0)
+#    kwant.plotter.map(sys,wave_density)
+#    #plt.show()
     
-    return fig
+
 
 
 if __name__ == "__main__":
-    fig = main()
-    # test_four()
+    main()
+
