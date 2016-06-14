a3168c693c588d22dfa9f1298e174bcee49696f8
rainson <rainson2012@qq.com>
2016-06-14 12:07:34 +0800

    graphene

--- graphene2nd.py
+++ graphene2nd.py
@@ -33,12 +33,17 @@ def plot_bands(lead,pars):
 
 
 
-def get_current(ev, sysvx, sysvy, hop_sites, pars):
+def get_core_matrix(sysvx, sysvy, hop_sites, pars):
     sites = sysvy.sites
     sites_index = [[sites.index(site[0]), sites.index(site[1])] for site in hop_sites]
     site_pos = np.array([(site[0].pos + site[1].pos) / 2.0 for site in hop_sites])
     vx_coomatrix = calculate.get_system_hamiltonian(sysvx, pars, sparse=True)
     vy_coomatrix = calculate.get_system_hamiltonian(sysvy, pars, sparse=True)
+    return site_pos[:,0],site_pos[:,1],sites_index, vx_coomatrix,vy_coomatrix
+
+
+def get_current(ev,sites_index, vx_coomatrix, vy_coomatrix ):
+
 
     ######构造ev coo_matrix(一种稀疏矩阵的存储方式) 这个地方是计算流密度的关键之处
     row = []
@@ -65,18 +70,15 @@ def get_current(ev, sysvx, sysvy, hop_sites, pars):
     vx = np.real(vx)
     vy = np.real(vy)
 
-    xs = site_pos[:, 0]
-    ys = site_pos[:, 1]
-    # data_save = np.array([site_pos[:,0],site_pos[:,1],vx,vy]).T
-    # np.savetxt("current_dens2.txt",data_save)
 
-    return xs, ys, vx, vy
+
+    return  vx, vy
 
 
 def plot_current_field(xs,ys,vx,vy):
     ###define grid
-    x_grid = np.linspace(xs.min(),xs.max(),500)
-    y_grid = np.linspace(ys.min(),ys.max(),500)
+    x_grid = np.linspace(xs.min(),xs.max(),200)
+    y_grid = np.linspace(ys.min(),ys.max(),200)
 
     vxs = griddata(xs, ys, vx, x_grid, y_grid, interp='linear')
     vys = griddata(xs, ys, vy, x_grid, y_grid, interp='linear')
@@ -89,6 +91,37 @@ def plot_current_field(xs,ys,vx,vy):
     plt.colorbar()
     plt.show()
 
+def get_current_with_lead(sys,sysvx,sysvy,hop_sites,pars):
+    wf = kwant.wave_function(sys, pars.EF, args=[pars])
+    eva = wf(0)
+    T = calculate.calculate_conductance(sys, pars.EF, 1, 0, pars)
+    print T, eva.shape[0]
+
+    xs, ys, sites_index, vx_coomatrix, vy_coomatrix = get_core_matrix(sysvx, sysvy, hop_sites, pars)
+
+    vx0 = 0;
+    ev0 = 0;
+    vy0 = 0;
+    for i in range(eva.shape[0]):
+        ev = eva[i, :]
+        vx, vy = get_current(ev, sites_index, vx_coomatrix, vy_coomatrix)
+
+        vx0 = vx + vx0
+        vy0 = vy + vy0
+        ev0 = ev0 + np.abs(ev) ** 2
+    return xs, ys, ev0, vx0, vy0
+
+def random_many_times(N,sys,sysvx,sysvy,hop_sites,pars):
+    ##############calculate randoms###########
+    ev0,vx0,vy0 = 0, 0, 0
+    for i in range(N):
+        xs, ys, ev, vx, vy = get_current_with_lead(sys,sysvx,sysvy,hop_sites,pars)
+        ev0 += ev
+        vx0 += vx
+        vy0 += vy
+
+    return xs, ys, ev0/N, vx0/N, vy0/N
+
 def main():
 
     Nx = 60
@@ -98,31 +131,26 @@ def main():
     # kwant.plot(sys)
     ####elecron wave####
     pars = SimplenameSpace()
-    pars.EL = 0.1
+    pars.EL = -0.1
     pars.ER = 0.1
     pars.phi = 0.007 * 2.3
     pars.EF = 0.0
-    # hams = calculate.get_system_hamiltonian(sys, pars, sparse=True)
-    # eigs, evs = eigsh(hams, which="SM", k=20)
-    ####get current####
-    # ev = evs[:, 0]
 
-    wf = kwant.wave_function(sys, pars.EF, args=[pars])
-    eva = wf(0)
-    T = calculate.calculate_conductance(sys,pars.EF,1,0,pars)
-    print T, eva.shape[0]
-    
-    vx0 = 0; ev0 = 0; vy0 = 0;
-    for i in range(eva.shape[0]):
-        ev = eva[i,:]
-        xs, ys, vx, vy = get_current(ev,sysvx,sysvy,hop_sites,pars)
 
-        vx0 = vx + vx0
-        vy0 = vy + vy0
-        ev0 = ev0 + np.abs(ev)**2
 
-    plot_current_field(xs, ys, vx0, vy0)
-    kwant.plotter.map(sys,ev0)
+    ########non_random
+    # pars.W = 0.0
+    # xs, ys, ev, vx, vy = get_current_with_lead(sys, sysvx, sysvy, hop_sites, pars)
+
+
+    ######randoms###
+    pars.W = 1.0
+    N = 10
+    random.seed(1000)
+    xs, ys, ev, vx, vy = random_many_times(N, sys, sysvx, sysvy, hop_sites, pars)
+
+    plot_current_field(xs, ys, vx, vy)
+    kwant.plotter.map(sys,ev)
 
 
 if __name__ == "__main__" :
